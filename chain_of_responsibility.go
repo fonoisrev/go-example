6ee4fbfa3cee7d6c10bcbd059b6d8cc5b957e6c1
Hang Wu <hang.wu@shopee.com>
2020-09-09 15:10:14 +0800

    函数风格的责任链模式

---
+++ chain_of_responsibility.go
@@ -0,0 +1,41 @@
+package main
+
+type Handler func(string) (string, bool)
+
+func MemoryGetValue(key string) (string, bool) {
+	var value string
+	var hasValue bool
+
+	// try get value from memory cache
+	return value, hasValue
+}
+
+func RedisGetValue(key string) (string, bool) {
+	var value string
+	var hasValue bool
+
+	// try get value from redis
+	return value, hasValue
+}
+
+func MysqlGetValue(key string) (string, bool) {
+	var value string
+	var hasValue bool
+
+	// try get value from mysql database
+	return value, hasValue
+}
+
+func GetValueByChain(key string, functions ...Handler) string {
+	for _, f := range functions {
+		value, ok := f(key)
+		if ok {
+			return value
+		}
+	}
+	return ""
+}
+
+func main() {
+	GetValueByChain("key", MemoryGetValue, RedisGetValue, MysqlGetValue)
+}
